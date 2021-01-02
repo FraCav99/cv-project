5b2542d635ceeb9fc1e1d0408e6f21e3e8bc9413
FraCav99 <MasterCaster36@outlook.it>
2021-01-02 18:12:22 +0100

    Add first component side menu and relative stylesheets

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Menu from './components/Menu';
+import './scss/main.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        
+      <div className="App">
+        <Menu />
       </div>
     )
   }
