7ac68b1f698c308a52827182ab63ec1a6cdd5221
FraCav99 <MasterCaster36@outlook.it>
2021-01-04 16:01:28 +0100

    Implemented logic for add data from modal

--- src/components/PersonalInfo.js
+++ src/components/PersonalInfo.js
@@ -2,6 +2,25 @@ import React, { Component } from 'react';
 import InfoImage from '../images/undraw_personal_information_962o.svg';
 
 class PersonalInfo extends Component {
+    constructor() {
+        super()
+
+        this.state = {
+            personalInfo: {
+                firstName: '',
+                lastName: '',
+                role: '',
+                address: '',
+                phoneNumber: '',
+                email: ''
+            },
+            errors: {},
+            isSubmitted: false
+        }
+
+        this.initialErrorsState = this.state.errors;
+    }
+
     toggleMenu = () => {
         const sideMenu = document.querySelector('.Menu');
         sideMenu.classList.toggle('side-menu-active');
@@ -12,13 +31,75 @@ class PersonalInfo extends Component {
         const InfoModal = document.querySelector('.personal-info-modal');
         const infoBody = document.querySelector('.info-body');
 
+        this.setState({errors: this.initialErrorsState});
+
         closeBtn.classList.toggle('not-visible');
         InfoModal.classList.toggle('not-visible');
         infoBody.classList.toggle('not-visible');
     }
 
+    handleChange = (ev) => {
+        this.setState({
+            personalInfo: {
+                ...this.state.personalInfo,    // leave other value unchanged
+                [ev.target.name]: ev.target.value,  // update the current value
+            }
+        });
+    }
+
+    validate = () => {
+        let isError = false;
+        const {personalInfo} = this.state;
+        const errors = {};
+        const regExp = /[a-zA-Z]/g; // check for characters in a string
+
+        if (!personalInfo.firstName) {
+            isError = true;
+            errors.firstNameError = 'Cannot be empty';
+        }
+        if (!personalInfo.lastName) {
+            isError = true;
+            errors.lastNameError = 'Cannot be empty';
+        }
+        if (!personalInfo.role) {
+            isError = true;
+            errors.roleError = 'Cannot be empty';
+        }
+        if (!personalInfo.address) {
+            isError = true;
+            errors.addressError = 'Cannot be empty';
+        }
+        if (!personalInfo.phoneNumber || !personalInfo.phoneNumber.length === 10 || regExp.test(personalInfo.phoneNumber)) {
+            isError = true;
+            errors.phoneNumberError = 'Cannot be empty and must be 10 digits long';
+        }
+        if (!personalInfo.email || !personalInfo.email.includes('@')) {
+            isError = true;
+            errors.emailError = 'Cannot be empty and must contain "@"';
+        }
+
+        // Add errors object to state if is not empty
+        if (Object.keys(errors).length > 0 && errors.constructor === Object) {
+            this.setState({errors});
+        }
+
+        return isError;
+    }
+
+    handleSubmit = (ev) => {
+        ev.preventDefault();
+        const {personalInfo} = this.state;
+        const err = this.validate();
+
+        if (!err) {
+            this.setState({personalInfo, isSubmitted: true});
+            this.toggleModal();
+        }
+    }
+
     render() {
-        const {toggleMenu, toggleModal} = this;
+        const {toggleMenu, toggleModal, handleChange, handleSubmit} = this;
+        const {errors, personalInfo} = this.state;
         return (
             <div className="PersonalInfo not-visible">
                 <div className="info-header">
@@ -46,35 +127,41 @@ class PersonalInfo extends Component {
                     <form>
                         <div>
                             <label>First Name</label>
-                            <input type="text" />
+                            {errors.firstNameError && <small className="error-pop-up">{errors.firstNameError}</small>}
+                            <input type="text" className="input-field" value={personalInfo.firstName} onChange={handleChange} name="firstName" />
                         </div>
 
                         <div>
                             <label>Last Name</label>
-                            <input type="text" />
+                            {errors.lastNameError && <small className="error-pop-up">{errors.lastNameError}</small>}
+                            <input type="text" className="input-field" value={personalInfo.lastName} onChange={handleChange} name="lastName" />
                         </div>
 
                         <div>
                             <label>Role</label>
-                            <input type="text" />
+                            {errors.roleError && <small className="error-pop-up">{errors.roleError}</small>}
+                            <input type="text" className="input-field" value={personalInfo.role} onChange={handleChange} name="role" />
                         </div>
 
                         <div>
                             <label>Address</label>
-                            <input type="text" />
+                            {errors.addressError && <small className="error-pop-up">{errors.addressError}</small>}
+                            <input type="text" className="input-field" value={personalInfo.address} onChange={handleChange} name="address" />
                         </div>
 
                         <div>
                             <label>Phone Number</label>
-                            <input type="text" />
+                            {errors.phoneNumberError && <small className="error-pop-up">{errors.phoneNumberError}</small>}
+                            <input type="text" name="phoneNumber"  maxLength="10" value={personalInfo.phoneNumber} onChange={handleChange} className="input-field" />
                         </div>
 
                         <div>
                             <label>Email</label>
-                            <input type="email" />
+                            {errors.emailError && <small className="error-pop-up">{errors.emailError}</small>}
+                            <input type="text" name="email" className="input-field" value={personalInfo.email} onChange={handleChange} />
                         </div>
 
-                        <button type="submit">Add info</button>
+                        <button type="submit" onClick={handleSubmit}>Add info</button>
                     </form>
                 </div>
             </div>
