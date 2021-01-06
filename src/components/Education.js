f4e118fbe59df36ddec239136876ded8780f76c8
FraCav99 <MasterCaster36@outlook.it>
2021-01-06 11:25:58 +0100

    Add edit functionality for Education component items

--- src/components/Education.js
+++ src/components/Education.js
@@ -7,6 +7,7 @@ class Education extends Component {
 
         this.state = {
             education: [],
+            eduItemId: '',
             tempEduItem: {},
             isEdited: false,
             errors: {}
@@ -29,9 +30,7 @@ class Education extends Component {
         inputFields.forEach(input => input.value = '');
 
         //Reset State
-        this.setState({
-            errors: {}
-        });
+        this.setState({errors: {}});
 
         closeBtn.classList.toggle('not-visible');
         EducationModal.classList.toggle('not-visible');
@@ -83,17 +82,17 @@ class Education extends Component {
         return isError;
     }
 
+    /* Add */
     addItem = (ev) => {
         ev.preventDefault();
         const eduItem = {};
         const err = this.validate();
 
         if (!err) {
-            // eduItem.id = this.state.education.length;
             eduItem.school = document.getElementById('school').value;
             eduItem.city = document.getElementById('city').value;
             eduItem.startDate = document.getElementById('startDate').value;
-            eduItem.endDate = document.getElementById('endDate').value || "Present";
+            eduItem.endDate = document.getElementById('endDate').value;
             eduItem.degree = document.getElementById('degree').value;
             eduItem.description = document.getElementById('description').value;
     
@@ -105,32 +104,83 @@ class Education extends Component {
         } else return;
     }
 
-    // editItem = (ev) => {
-    //     ev.preventDefault();
-    // }
+    /* Edit */
+    toggleEditModal = (ev) => {
+        const {education, isEdited} = this.state;
+        const itemId = ev.currentTarget.parentNode.parentNode.parentNode.getAttribute('id');
 
-    filterArray(arr, index) {
-        if (index > -1) {
-          arr.splice(index, 1);
+        if (isEdited) {
+            this.setState({
+                tempEduItem: {},
+                isEdited: false,
+                eduItemId: ''
+            });
+        } else {
+            this.setState({
+                tempEduItem: education[itemId],
+                isEdited: true,
+                eduItemId: itemId
+            });
         }
-    
+
+        this.toggleModal();
+    }
+
+    handleEditChange = ev => {
+        this.setState({
+            tempEduItem: {
+                ...this.state.tempEduItem,
+                [ev.target.id]: ev.target.value
+            }
+        });
+    }
+
+    editArray = (arr, id, editedObj) => {
+        arr[id] = editedObj;
+        return arr;
+    }
+
+    editItem = (ev) => {
+        ev.preventDefault();
+        const err = this.validate();
+
+        if (!err) {
+            const {education, tempEduItem, eduItemId} = this.state;
+            this.setState({
+                isEdited: false,
+                education: this.editArray(education, eduItemId, tempEduItem)
+            });
+            this.toggleModal();
+        } else return;
+    }
+
+    /* Delete */
+    filterArray(arr, index) {
+        index > -1 && arr.splice(index, 1);
         return arr;
     }
 
     deleteItem = (ev) => {
         const {education} = this.state;
         const itemId = ev.currentTarget.parentNode.parentNode.parentNode.getAttribute('id');
-        console.log(itemId);
         this.setState({
             education: this.filterArray(education, itemId)
         });
     }
 
     render() {
-        const {toggleMenu, toggleModal, addItem, deleteItem} = this;
-        const {errors, education} = this.state;
+        const {
+            toggleMenu,
+            toggleModal,
+            toggleEditModal,
+            addItem,
+            handleEditChange,
+            editItem,
+            deleteItem
+        } = this;
+        const {errors, education, tempEduItem, isEdited} = this.state;
         return (
-            <div className="Education">
+            <div className="Education not-visible">
                 <div className="education-header">
                     <div className="burger-menu" onClick={toggleMenu}>
                         <span className="line line-1"></span>
@@ -138,9 +188,15 @@ class Education extends Component {
                         <span className="line line-3"></span>
                     </div>
                     <h2>Education</h2>
-                    <div className="close-button-education not-visible" onClick={toggleModal}>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"/></svg>
-                    </div>
+                    {
+                        isEdited ?
+                        <div className="close-button-education not-visible" onClick={toggleEditModal}>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"/></svg>
+                        </div> :
+                        <div className="close-button-education not-visible" onClick={toggleModal}>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"/></svg>
+                        </div>
+                    }
                 </div>
 
             
@@ -150,9 +206,9 @@ class Education extends Component {
                             return (
                                 <div key={index} id={index} className="education-item">
                                 <div className="item-header">
-                                    <span>{item.startDate}-{item.endDate}</span>
+                                    <span>{item.startDate}-{item.endDate || 'Present'}</span>
                                     <div className="action-buttons">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M7.127 22.564l-7.126 1.436 1.438-7.125 5.688 5.689zm-4.274-7.104l5.688 5.689 15.46-15.46-5.689-5.689-15.459 15.46z"/></svg>
+                                        <svg onClick={toggleEditModal} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M7.127 22.564l-7.126 1.436 1.438-7.125 5.688 5.689zm-4.274-7.104l5.688 5.689 15.46-15.46-5.689-5.689-15.459 15.46z"/></svg>
                                         <svg onClick={deleteItem} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"/></svg>
                                     </div>
                                 </div>
@@ -183,20 +239,32 @@ class Education extends Component {
                         <div>
                             <label>School</label>
                             {errors.school && <small className="error-pop-up">{errors.school}</small>}
-                            <input type="text" id="school" className="input-field"/>
+                            {
+                                isEdited ? 
+                                <input type="text" id="school" onChange={handleEditChange} value={tempEduItem.school} className="input-field"/> :
+                                <input type="text" id="school" className="input-field"/>
+                            }                            
                         </div>
 
                         <div>
                             <label>City</label>
                             {errors.city && <small className="error-pop-up">{errors.city}</small>}
-                            <input type="text" id="city" className="input-field"/>
+                            {
+                                isEdited ?
+                                <input type="text" id="city" onChange={handleEditChange} value={tempEduItem.city} className="input-field"/> :
+                                <input type="text" id="city" className="input-field"/>
+                            }
                         </div>
 
                         <div className="period">
                             <div>
                                 <label>From</label>
                                 {errors.startDate && <small className="error-pop-up">{errors.startDate}</small>}
-                                <input type="text" id="startDate" className="input-field"/>
+                                {
+                                    isEdited ?
+                                    <input type="text" id="startDate" onChange={handleEditChange} value={tempEduItem.startDate} className="input-field"/> :
+                                    <input type="text" id="startDate" className="input-field"/>
+                                }
                             </div>
                             <div>
                                 <label>
@@ -204,34 +272,52 @@ class Education extends Component {
                                     <small> ("Present" if empty)</small>
                                 </label>
                                 {errors.endDate && <small className="error-pop-up">{errors.endDate}</small>}
-                                <input type="text" id="endDate" className="input-field"/>
+                                {
+                                    isEdited ?
+                                    <input 
+                                        type="text" 
+                                        id="endDate"
+                                        onChange={handleEditChange}
+                                        value={tempEduItem.endDate !== 'Present' ? tempEduItem.endDate : null} 
+                                        className="input-field"
+                                    /> :
+                                    <input type="text" id="endDate" className="input-field"/>
+                                }
                             </div>
                         </div>
 
                         <div>
                             <label>Degree</label>
                             {errors.degree && <small className="error-pop-up">{errors.degree}</small>}
-                            <input type="text" id="degree" className="input-field"/>
+                            {
+                                isEdited ?
+                                <input type="text" id="degree" onChange={handleEditChange} value={tempEduItem.degree} className="input-field"/> :
+                                <input type="text" id="degree" className="input-field"/>
+                            }         
                         </div>
 
                         <div className="description">
                             <label>Description</label>
-                            <textarea rows="7" id="description" className="input-field"></textarea>
+                            {
+                                isEdited ?
+                                <textarea rows="7" id="description" onChange={handleEditChange} value={tempEduItem.description} className="input-field"></textarea> :
+                                <textarea rows="7" id="description" className="input-field"></textarea>
+                            }
                         </div>
-
-                        <button type="submit" onClick={addItem}>Add Education</button>
+                        {
+                            isEdited ?
+                            <button type="submit" onClick={editItem}>Edit Education</button> :
+                            <button type="submit" onClick={addItem}>Add Education</button>
+                        }
                     </form>
                 </div>
 
-                {education.length > 0 ? (
+                {
+                    education.length > 0 && 
                     <button className="add-education-btn" onClick={toggleModal}>
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M24 10h-10v-10h-4v10h-10v4h10v10h4v-10h10z"/></svg>
                     </button>
-                ) : (
-                    <button className="add-education-btn not-visible">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M24 10h-10v-10h-4v10h-10v4h10v10h4v-10h10z"/></svg>
-                    </button>
-                )}
+                }
             </div>
         )
     }
